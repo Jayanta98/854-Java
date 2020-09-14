d3926bdd7dac6264cab91f31dcc2ebf3788c8e25
Jayanta <jg.16u10555@btech.nitdgp.ac.in>
2020-09-14 12:28:44 +0530

    new student class created

---
+++ Demo/src/main/java/com/jayanta/Demo/Student.java
@@ -0,0 +1,23 @@
+package com.jayanta.Demo;
+
+public class Student {
+	
+	private  String sname;
+	private int sroll;
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public int getSroll() {
+		return sroll;
+	}
+	public void setSroll(int sroll) {
+		this.sroll = sroll;
+	}
+	
+	
+	
+
+}
